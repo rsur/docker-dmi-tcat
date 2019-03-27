d7cde7dd8f28130d7219fea32a90044a96d37a29
Rendra Suroso <rs@cogsci.bandungfe.net>
2019-03-27 22:23:13 +0700

    Mount tcat cache to host.

--- start.sh
+++ start.sh
@@ -2,5 +2,8 @@
 
 /usr/sbin/service php5-fpm start &&
 /usr/sbin/service supervisor start &>/dev/null &&
+for i in proc logs analysis/cache; do
+	chown www-data:www-data /var/www/html/$i
+done
 /usr/sbin/nginx -g "daemon off;"
 
